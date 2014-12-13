e07a45396482bf3ef931c217961d0eb57a4a0ffd
xatshepsut <>
2014-12-13 17:46:41 +0400

    Initial Commit

---
+++ Vostan_Natoive_iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Vostan_Natoive_iOS
+//
+//  Created by Macadamian User on 12/13/14.
+//  Copyright (c) 2014 AUA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
